aaa1ebd2d30a4b60cfe3927097284ea364ec947b
omerkeskin <omer.keskin.deu@hotmail.com>
2019-04-07 14:06:32 +0300

    Base Source files and pom config.

---
+++ src/main/java/com/omerkeskin/jpa/postgreexample/PostgreExampleApplication.java
@@ -0,0 +1,13 @@
+package com.omerkeskin.jpa.postgreexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgreExampleApplication.class, args);
+    }
+
+}
